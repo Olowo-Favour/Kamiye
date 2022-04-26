5af3c03e0dd97d725c9c92bbaf776b49270a7c88
Olowo-Favour <104360738+Olowo-Favour@users.noreply.github.com>
2022-04-26 05:59:52 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+def myname():
+    return("Kamiye")
+
+print(myname())
+
+
+def myname(number=600):
+    return (number*number)
+
+print(myname())
+
+x = {"name": "Kamiye", "Age": "2", "Location": "IbadanCITY"}
+print (x["name"])
+print (x["Location"])
+print (x["Age"])
+print (type(x))
+
+y = [12, 13, 14, 15, "Kamiye"]
+print (y[4])
+print (type(y))
+
+myname = input("What is your name:")
+myname = input("Where do you live:")
+myname = input("How old are you:")
+myname = input("Where do you school:")
+myname = input("Fill in the form with yes or no:")
+print (myname)
+
